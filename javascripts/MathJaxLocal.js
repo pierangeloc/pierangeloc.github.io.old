6d132cc865fa61428d65f6d8a25c0558337a9e2d
pierangeloc <pierangeloc@gmail.com>
2015-11-28 14:59:44 +0100

    Site updated at 2015-11-28 13:59:44 UTC

---
+++ javascripts/MathJaxLocal.js
@@ -0,0 +1,18 @@
+MathJax.Hub.Config({
+    jax: ["input/TeX","output/HTML-CSS"],
+    extensions: ["tex2jax.js","MathMenu.js","MathZoom.js"],
+    tex2jax:
+        {
+            inlineMath: [ ['$','$'], ['\\(','\\)'] ],
+            displayMath: [ ['$$','$$'], ['\\[','\\]'] ],
+            skipTags: ["script","noscript","style","textarea","pre","code"],
+            processEscapes: true
+        },
+    TeX:
+        {
+            equationNumbers: { autoNumber: "AMS" },
+            TagSide: "left",
+
+        },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+    });
